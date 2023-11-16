e014e52a7d2134a0397bd3ff3d15004946f95cb6
Marieli Teixeira <142545847+the-marielis@users.noreply.github.com>
2023-11-16 20:35:42 -0300

    Update function.js
    ajuste no layout e funcionamento da lógica do código

--- function.js
+++ function.js
@@ -1,20 +1,23 @@
 var numeroSecreto = parseInt(Math.random() * 1000) +1;
 var cont = 1;
 var tentativa = 'chute';
-var tentativasRestantes = 10;
+var numeromaximo = prompt("Vamos começar um Jogo!! \nCom quantas tentativas você descobre o número secreto entre 0 e 1000?");
+var tentativasRestantes = numeromaximo;
 
-while (chute != numeroSecreto || cont <= tentativasRestantes) {    
-    alert(`Vamos começar o jogo! Você tem ${tentativasRestantes} tentativas`)   
+
+while (chute != numeroSecreto && cont <= numeromaximo) {    
     var chute = prompt('Digite um número entre 0 e 1000');
     if (chute == numeroSecreto) {
         alert(`Você acertou com ${cont} ${tentativa}`);
     } else if (chute < numeroSecreto) {
-        alert(`Você errou! O número secreto é maior que ${chute} :( Você já deu ${cont} ${tentativa}`);
+        alert(`Você errou! O número secreto é maior que ${chute} :( \nVocê já deu ${cont} ${tentativa}`);
     } else if (chute > numeroSecreto) {
-        alert(`Você errou! O número secreto é menor que  ${chute} :( Você já deu ${cont} ${tentativa}`);    
+        alert(`Você errou! O número secreto é menor que  ${chute} :( \nVocê já deu ${cont} ${tentativa}`);    
     }
+
     tentativasRestantes--; 
     cont++;
+
     if (cont == 1) {
         tentativa = 'chute';
     } else {
